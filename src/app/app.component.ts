7fd689e276cabdd29f26eb7782c961878bc7b1be
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-07-31 18:45:27 -0300

    configurando aplicação inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MainLayoutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+    { path: '', pathMatch:'full', redirectTo: 'home'},
+    {path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent)
+    },
+    {path: 'clientes',
+        loadComponent: () => import('./components/clientes/clientes.component').then(c => c.ClientesComponent)
+    },
+    {path: 'produtos',
+        loadComponent: () => import('./components/produtos/produtos.component').then(c => c.ProdutosComponent)
+    },
+    {path: 'vendas',
+        loadComponent: () => import('./components/vendas/vendas.component').then(c => c.VendasComponent)
+    },
+    {path: 'fornecedores',
+        loadComponent: () => import('./components/fornecedores/fornecedores.component').then(c => c.FornecedoresComponent)
+    },
+    {path: 'compras',
+        loadComponent: () => import('./components/compras/compras.component').then(c => c.ComprasComponent)
+    }
+];
