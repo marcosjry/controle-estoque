c5857bc16ff94287507be2eb48703cb48c37c0b6
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-08-07 11:47:59 -0300

    reestruturando componentes de produtos

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -2,10 +2,10 @@ import { ProdutosService } from './services/produtos.service';
 import { Component } from '@angular/core';
 import { ProdutosHeaderComponent } from './components/produtos-header/produtos-header.component';
 import { ProdutosSearchComponent } from './components/produtos-search/produtos-search.component';
-import { ProdutosTableListComponent } from './components/produtos-table-list/produtos-table-list.component';
 import { CommonModule, NgIf } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Product } from './model/produto';
+import { ProdutosTableListComponent } from '../../shared/components/produtos-table-list/produtos-table-list.component';
 
 
 @Component({
@@ -22,10 +22,15 @@ import { Product } from './model/produto';
   styleUrl: './produtos.component.scss'
 })
 export class ProdutosComponent {
-  observableProduct!: Observable<Product[]>;
 
+  observableProduct!: Observable<Product[]>;
+  columns!: Observable<String[]>;
   constructor (private produtosService: ProdutosService) {
     this.observableProduct = this.produtosService.list();
+    
   }
 
+  ngOnInit() {
+    this.observableProduct.subscribe(products => console.log(products))
+  }
 }
